4960951b8a927e094730ff08f4c33c9c15870d79
Gulnar Mammadli <gulnar.mammadli@pipedrive.com>
2025-02-08 00:22:45 +0200

    initial commit

---
+++ src/Pizza.jsx
@@ -0,0 +1,9 @@
+export const Pizza = (props) => {
+  return (
+    <div className="pizza">
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
+    </div>
+  );
+};
